77a59d9c321a2e724d824ba62a3b7289f21afbae
Humberto Galindo <Hgalindo@uade.edu.ar>
2026-04-21 23:37:38 -0300

    Estructura principal del menu, primera version

---
+++ main.py
@@ -0,0 +1,116 @@
+def leer_entero(mensaje):
+    '''Objetivo: verificar que lo ingrersado por el usuario sea un numero entero
+       Parametros: un valor x que haya ingresado el usuario
+       Salida: un numero entero
+       '''
+    valor= input(mensaje)
+    
+    while not valor.isdigit():
+       
+        print("ERROR, por favor ingrese una opción valida")
+        valor= input(mensaje)
+    
+    return int(valor)
+    
+def menu_admin():
+    '''Objetivo: mostrar el menu del administrador
+       Parametros: ninguno
+       Salida: menu del administrador
+    '''
+    continuar= True
+    
+    while continuar:
+        
+        print("------------------------------------------")
+        print("            MENU ADMINISTRADOR            ")
+        print("------------------------------------------")
+        print()
+        print(" 1 --> Gestión de Inventario")
+        print(" 2 --> Salir")
+        
+        opcion= leer_entero(input("Selecione una opción: "))
+        
+        if opcion == "1":
+            print("hola")
+            
+        elif opcion == "2":
+            continuar=False
+            main()
+        
+        else:
+            print("Opción Invalida")
+            
+def menu_cliente():
+    '''Objetivo: mostrar el menu del usuario
+       Parametros: ninguno
+       Salida: menu del usuario
+    '''
+    continuar= True
+    
+    while continuar:
+        
+        print("------------------------------------------")
+        print("               MENU CLIENTE               ")
+        print("------------------------------------------")
+        print()
+        print(" 1 --> Catálogo Completo")
+        print(" 2 --> Búsqueda por Categoria")
+        print(" 3 --> Agregar Producto al Carrito")
+        print(" 4 --> Ver Carrito y Total")
+        print(" 5 --> Salir")
+        
+        opcion= leer_entero(input("Seleccione una opción: "))
+        
+        if opcion == "1":
+            print("catalogo")
+        
+        elif opcion == "2":
+            print("busqueda")
+            
+        elif opcion == "3":
+            print("agregar carrito")
+            
+        elif opcion == "4":
+            print("Ver carrito")
+        
+        elif opcion == "5":
+            main()
+            
+        else:
+            print("Opción Invalida")
+            
+def main():
+    '''Objetivo: mostrar el programa funcional
+       Parametros: ninguno
+       Salida: programa principal funcional
+    '''
+    continuar= True  
+    
+    while continuar:
+          
+        print("------------------------------------------")
+        print("           E-COMMERCE BETA 1.O            ")
+        print("------------------------------------------")
+        print()
+        print(" 1 --> Ingresar como Administrador")
+        print(" 2 --> Ingresar como Cliente")
+        print(" 3 --> Salir")
+        
+        opcion= leer_entero(input("Seleccione una opción: "))
+        
+        if opcion == "1":
+            menu_admin()
+        
+        elif opcion == "2":
+            menu_cliente()
+            
+        elif opcion == "3":
+            continuar=False
+            print("Gracias por confiar en nosotros, vuelva pronto")
+        
+        else:
+            print("Opción Invalida")
+        
+# Punto de entrada del programa
+if __name__ == "__main__":
+    main()
