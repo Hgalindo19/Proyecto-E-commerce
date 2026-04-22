43b10082aece1227a6d83d9d710c4008950c0b2b
Ivan <ivcarrizo@uade.edu.ar>
2026-04-22 13:44:43 -0300

    Cambios en la rama

--- Inventario.py
+++ Inventario.py
@@ -1,15 +1,46 @@
 """Creacion del Inventario con busqueda por ID"""
-productos = {
-    1: {"Nombre": "Monitor", "Precio": 250000, "Stock": 6},
-    2: {"Nombre": "Teclado Mecanico", "Precio": 45000, "Stock": 20},
-    3: {"Nombre": "Mouse","Precio": 20000, "Stock": 10},
-    4: {"Nombre": "Auriculares","Precio": 35000, "Stock":15}
-}
+lista_producto=[]
+lista_precios=[]
+lista_stock=[]
+lista_categoria=[]
+def alta_producto():
+    print("Agrege un nuevo producto")
+    nombre=input ("Nombre del producto: ")
+    categoria= input ("Catergoria: ")
+    while True:
+        try:
+            precio = float(input("Precio: "))
+            if precio > 0:
+                break
+            else:
+                print("El precio no debe ser negativo. Intente denuevo")
+        except ValueError:
+            print("Ingrese un numero valido.")
+    
+    lista_producto.append(nombre)
+    lista_precios.append(precio)
+    lista_stock.append(stock)
+    lista_categoria.append(categoria)
+                
 
-def Agregar_producto(id_producto,nombre,stock,precio):
+
+def Agregar_producto(id_producto,nombre,stock,precio,categoria):
     productos[id_producto]= {
         "Nombre": nombre,
         "Precio": precio,
+        "Categoria": categoria,
         "Stock": stock
     }
-    
+
+"""""Objetivo: Actualizar Precios de forma masiva 
+    Parametro: Precios, Iva
+    Salido: Precios Actualizados"""
+def actualizar_precios():
+    print("Actualizacion de Precios")
+
+while True:
+    try:
+        porcentaje = float(input("Ingrese cuanto quiere aumentar: "))
+        confirmar= input("¿Aplicar? {porcentaje}% a todos los precios (s/n): ").lower()
+        if confirmar == "s":
+            for i in range(len())
