13e3edc7b6c080735dcd89f1fc94d05584fa0891
bauticomp23 <bautistacompaired@gmail.com>
2026-04-22 22:23:35 -0300

    Add files via upload

---
+++ Búsqueda y lógistica.py
@@ -0,0 +1,44 @@
+# Listas
+
+productos = ["Remera", "Celular", "Auto"]
+categorias = ["Ropa", "Tecnología", "Vehículos"]
+precios = [15000, 250000, 800000]
+
+# Función mostrar productos
+
+def mostrar_productos():
+    for i in range(3):
+        print(productos[i], categorias[i], precios[i])
+        
+# Funció buscar categoría
+
+def buscar_categoria():
+    
+    buscar = input("Ingrese categoría: ")
+    
+    for i in range(3):
+        if buscar == categorias[i]:
+            print(productos[i], precios[i])
+            
+            
+# Menú
+
+def menu():
+    
+    opcion = 0
+    
+    while opcion != 3:
+        
+        print("1 - Ver productos")
+        print("2 - Buscar categoría")
+        print("3 - Salir")
+        
+        opcion = int(input("Ingrese opción: "))
+        
+        if opcion == 1:
+            mostrar_productos()
+            
+        elif opcion == 2:
+            buscar_categoria()
+            
+menu()
